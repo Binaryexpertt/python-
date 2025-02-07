fb10b85878b983fba50cfde3dc692baf15c3e874
Alp Cagri HASIPEK <alphasipek@gmail.com>
2025-02-07 22:34:13 +0300

    Add files via upload

---
+++ NameArray.py
@@ -0,0 +1,19 @@
+"""Flowchart that transfers a name entered from the screen to the H array as shown below
+Input Screen                      Output Screen
+Enter Name:Mark                   m
+                                  a a
+                                  r r r
+                                  k k k k                             """
+    
+s1="mark"
+s2=s1[0]
+s3=s1[1]
+s4=s1[2]
+s5=s1[3]
+
+print("\n"+s2)
+print("\n"+s3+ " " + s3)
+print("\n"+s4+" "+s4+" "+s4)
+print("\n"+s5+" "+s5+" "+s5+" "+s5)
+                                  
+                                  
